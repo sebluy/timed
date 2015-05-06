04a90a52e7c82bbdc34e55244665114d90449574
Sebastian Luy <sebastian.a.luy@gmail.com>
2015-05-06 15:59:28 -0400

    Clean up presentation with bootstrap

--- src-cljs/bed_time/bed_times.cljs
+++ src-cljs/bed_time/bed_times.cljs
@@ -4,42 +4,58 @@
 
 (defn bed-times-updater [bed-times]
   (fn [response]
-    (reset! bed-times (into [] (map (fn [time]
-                                      (.toUTCString (time :time)))
+    (reset! bed-times (into [] (map (fn [time] (time :time))
                                     (response :bed-times))))))
 
 (defn get-bed-times [bed-times]
+  (println "getting bed-times...")
   (GET "/bed-times" {:handler (bed-times-updater bed-times)
                      :response-format :edn}))
 
 (defn go-to-bed-button [bed-times]
-  [:input {:type "button"
-           :value "Go to bed!"
-           :on-click (fn [] (swap!
-                              bed-times
-                              #(conj % (.toUTCString (js/Date.)))))}])
+  [:input.btn.btn-large.btn-success
+   {:type "button"
+    :value "Go to bed!"
+    :on-click (fn [] (swap!
+                       bed-times
+                       #(conj % (js/Date.))))}])
 
 (defn remove-at-index [v n]
   (vec (concat (subvec v 0 n) (subvec v (inc n) (count v)))))
 
 (defn delete-button [n bed-times]
-  [:input {:type "button"
-           :value "Delete!"
-           :on-click (fn [] (swap! bed-times #(remove-at-index % n)))}])
+  [:input.btn.btn-sm.btn-danger
+   {:type "button"
+    :value "Delete!"
+    :on-click (fn [] (swap! bed-times #(remove-at-index % n)))}])
+
+(defn show-bed-time [bed-times current-bed-times n]
+  (let [bed-time (get current-bed-times n)]
+    ^{:key n}
+    [:tr
+     [:td (.toLocaleDateString bed-time)]
+     [:td (.toLocaleTimeString bed-time)]
+     [:td (delete-button n bed-times)]]))
 
 (defn bed-time-list [bed-times]
-  [:ul
-   (let [current-bed-times @bed-times]
-     (for [n (range (count current-bed-times))]
-       ^{:key n} [:li
-                  (get current-bed-times n)
-                  (delete-button n bed-times)]))])
+  [:table.table
+   [:thead [:tr [:td "Date"] [:td "Time"]]]
+   [:tbody
+    (let [current-bed-times @bed-times]
+      (for [n (range (count current-bed-times))]
+        (show-bed-time bed-times current-bed-times n)))]])
+
+(defn header [bed-times]
+  [:div.page-header
+   [:h1 "Bed Times"
+    [:div.pull-right
+      (go-to-bed-button bed-times)]]])
 
 (defn bed-times-page []
   (let [bed-times (atom [])]
     (get-bed-times bed-times)
     (fn []
-      [:div
-       (go-to-bed-button bed-times)
+      [:div.col-md-6.col-md-offset-3
+       (header bed-times)
        (bed-time-list bed-times)])))
 
